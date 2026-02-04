d76e64af7d9a6d39d62cb347413dc06e5403c18f
Dan Rodney <chett.tiller@gmail.com>
2026-02-04 17:05:03 -0500

    init

---
+++ cat-memes.js
@@ -0,0 +1,47 @@
+const catMemes = [
+    {
+        id: 1,
+        name: "Quarantine Cat",
+        img_url: "https://i.chzbgr.com/full/9517778944/h30875976/cat-self-quarantined-day-6-laughing-at-my-own-jokes"
+    },
+    {
+        id: 2,
+        name: "Starving Cat",
+        img_url: "https://inspirationfeed.com/wp-content/uploads/2020/05/Cat-Meme-7.jpg"
+    },
+    {
+        id: 3,
+        name: "Confused Cat",
+        img_url: "https://i.pinimg.com/originals/cb/92/98/cb929817f2356ce431c18a4ed52101e1.jpg"
+    },
+    {
+        id: 4,
+        name: "Omewnous Cat",
+        img_url: "https://i.pinimg.com/736x/79/df/b4/79dfb400c36411ece71e14d5cc184af9.jpg"
+    },
+    {
+        id: 5,
+        name: "Professor Cat",
+        img_url: "https://miro.medium.com/max/418/0*EWewK-mIhbuXROpr.png"
+    },
+    {
+        id: 7,
+        name: "Don't Be Angry At The Cat",
+        img_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/womanyellingcat-1573233850.jpg?resize=480:*"
+    },
+    {
+        id: 8,
+        name: "Aliens Cat",
+        img_url: "https://i.pinimg.com/originals/7b/39/e6/7b39e69957ae119a138103ae7df1ab35.jpg",
+    },
+    {
+        id: 9,
+        name: "Election Cat",
+        img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIxV6WpZqBVojoW-7TGJI38-MGXRP5Gqq8kQ&usqp=CAU",
+    },
+    {
+        id: 10,
+        name: "Space Cat",
+        img_url: "https://cdn.buttercms.com/djbzxPmAS16W30LE8YLp",
+    }
+  ]
