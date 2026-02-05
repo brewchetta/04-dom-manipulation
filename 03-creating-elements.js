f764c0b91a13a22d2a86a9c8d996df50781454a1
Dan Rodney <chett.tiller@gmail.com>
2026-02-05 17:00:07 -0500

    dom exercises

--- 03-creating-elements.js
+++ 03-creating-elements.js
@@ -43,18 +43,74 @@ function addMemeName( name ) {
 
 catMemes.forEach( memeObj => addMemeName(memeObj.name) )
 
-// ADDITIONAL EXERCISES
+// ADDITIONAL EXERCISES - RETURN AT 4:20 EST
 
 // 1. Create a <nav> element and PREPEND it to the page.
 
+const nav = document.createElement("nav")
+document.body.prepend( nav )
+
 // 2. Create three <a> elements, "Home", "About", "Contact Us". These should all have an href="#" so that they link back to the current page. Append them to the <nav> element.
+
+const a1 = document.createElement("a")
+const a2 = document.createElement("a")
+const a3 = document.createElement("a")
+
+a1.textContent = "Home"
+a2.textContent = "About"
+a3.textContent = "Contact Us"
+
+a1.href = "#"
+a2.href = "#"
+a3.href = "#"
+
+nav.append(a1, a2, a3)
 
 // 3. Create a <button> that reads "Replace All Images" and append it to the page.
 
+const replaceButton = document.createElement("button")
+replaceButton.textContent = "Replace All Images"
+document.body.append(replaceButton)
+
 // 4. When the <button> with "Replace All Images" is clicked, it replaces the `src` attribute for every image on the page with an image of your choice.
 
-// 5. Create a <footer> element. Give it a display of "grid" and a gridTemplateColumns of "1fr 1fr". Append it to the body.
+function replaceCatImage( img ) {
+    img.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHhodW84enk4ejB1azRvdjA4NWoycWNlMW04am1uY3lnemlkbDNsdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/h6AMD4GXFxO2k/giphy.gif"
+}
+
+function replaceAllCatImages() {
+    const allImages = document.querySelectorAll("img")
+    allImages.forEach( replaceCatImage )
+}
+
+replaceButton.addEventListener( "click", replaceAllCatImages )
+
+// 5. Create a <footer> element. Give it a display of "grid" and a gridTemplateColumns of "1fr 1fr". Append it to the body. Feel free to add additional styling as you see fit.
+
+const footer = document.createElement("footer")
+footer.style.display = "grid"
+footer.style.gridTemplateColumns = "1fr 1fr"
+document.body.append( footer )
 
 // 6. Create two <p> elements. Inside one add copyright information and inside the other add a phone number (make one up). Append both of them to the <footer> element.
 
-// RETURN AT 4:20 EST
+const p1 = document.createElement("p")
+const p2 = document.createElement("p")
+
+p1.textContent = "© Cat Memes Super Site 2026"
+p2.textContent = "+1 999 888 7777"
+
+footer.append(p1, p2)
+
+// BONUS: Create a <p> element named "clock" and prepend it to the page. Use setInterval to run a callback each second (1000ms / 1s). Each second change the element's `textContent` to the current datetime.
+// For the current datetime use this: new Date().toLocaleString("en-US")
+
+const clock = document.createElement("p")
+clock.textContent = new Date().toLocaleString("en-US")
+document.body.prepend( clock )
+
+function tickTock() {
+    clock.textContent = new Date().toLocaleString("en-US")
+}
+
+setInterval(tickTock, 1000)
