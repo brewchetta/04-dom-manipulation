89ee308e085067566db9b5507b4ea3fcc6f78a54
Dan Rodney <chett.tiller@gmail.com>
2026-02-05 13:04:52 -0500

    checkpoint-1

--- 01-selectors.js
+++ 01-selectors.js
@@ -1,1 +1,93 @@
 // SELECTORS
+
+// DOM - Document Object Model
+// vs
+// HTML
+
+// HTML is a blueprint, the DOM is a living version that has been built off the HTML
+
+// the DOM is a tree with nodes
+// - document --> this is the root of the whole tree
+//     - html
+//         - head
+//             - meta
+//             - meta
+//             - title
+//             - link
+//         - body
+//             - h1
+//             - p
+//             - p - .removeable-item
+//             - p - .removeable-item
+//             - p - .removeable-item
+//             - ul
+//              - li
+//              - li
+//              - li
+//              - li
+//              - li
+//              - li
+//             - img
+//             - div 
+
+// selecting --> reading the page, finding elements
+
+// querySelector returns the FIRST element it finds that matches
+
+const mainHeader = document.querySelector("h1")
+
+const noteToRemove = document.querySelector("#note-to-remove")
+
+const pTagWithClass = document.querySelector(".removeable-item")
+
+// querySelectorAll gets all items that match
+
+const allPTagsWithClass = document.querySelectorAll(".removeable-item")
+
+// returns a NodeList which is similar to an array
+
+// manipulating styles...
+
+mainHeader.style.color = "blue"
+mainHeader.style.textAlign = "center"
+
+noteToRemove.textContent = "Where cats rule the world!"
+noteToRemove.style.textAlign = "center"
+
+// change things about an img
+
+const gifURL = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGpmcG42NmF3NXFjZHV2eTRvZnJ0NXVzM3licGlzc3Fndm96OXhtMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/h6AMD4GXFxO2k/giphy.gif"
+
+const headlinerImg = document.querySelector("#headliner-meme")
+
+headlinerImg.src = gifURL
+headlinerImg.alt = "cat on a roomba"
+
+// remove elements from the page
+
+noteToRemove.remove()
+
+allPTagsWithClass.forEach( item => item.remove() )
+
+// EXERCISES
+
+// 1. Create an <h2> beneath the <h1>. Select the <h2> and change its `textContent` to be "Where cats rule the world". Also change its `color` to "red". Additionally center the text.
+
+const subTitle = document.querySelector("#sub-title")
+subTitle.textContent = "Where cats rule the world"
+subTitle.style.color = "red"
+subTitle.style.textAlign = "center"
+
+// 2. Create a <ul> with a series of <li> items detailing things about cats. Select each of the <li> elements and remove them.
+
+// const removeList = document.querySelectorAll("#remove-list li")
+// removeList.remove()
+
+const removeListItems = document.querySelectorAll("#remove-list li")
+removeListItems.forEach( item => item.remove() )
+
+// 3. Find a cat meme on the internet and change the `#headliner-meme` element so that its `src` is that new cat meme.
+
+const headlinerMeme = document.querySelector("#headliner-meme")
+
+headlinerMeme.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHBsZHhxaXFobnVjaDNzdTdtbmJrNzlueHc1djZoMzBneDhxbHZxeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ngs9Nqu0pqkec/giphy.gif"
--- 02-handling-events.js
+++ 02-handling-events.js
@@ -1,1 +1,80 @@
 // HANDLING EVENTS
+
+// GOAL: build a stopwatch
+
+// EVENTS
+
+const catHelloButton = document.querySelector("#cat-hello-button")
+catHelloButton.addEventListener( "click", () => alert("MEOW") )
+
+// event listener is waiting for a specific event to happen and then it calls a callback function
+
+// get the stopwatch button and text
+const stopwatchButton = document.querySelector("#stopwatch-button")
+const stopwatchText = document.querySelector("#stopwatch-text")
+
+// make variables for the time and whether it's running
+// state - a changeable variable that tracks something important
+let stopwatchTime = 0
+let isStopwatchRunning = false
+
+// add event listener which starts the process when the button is clicked
+stopwatchButton.addEventListener("click", startStopwatch)
+
+// setInterval
+// two parts - callback, time between calls
+
+function startStopwatch() {
+    // checking to make sure the stopwatch isn't running yet
+    if ( isStopwatchRunning === false ) {
+        // if false let's run it
+        setInterval(incrementStopwatch, 1000)
+        // since the stopwatch is starting we change the value here to true so that we can't attempt to start it again
+        isStopwatchRunning = true
+    }
+}
+
+// this function happens every 1000ms when the stopwatch has started
+function incrementStopwatch() {
+    // increment the variable
+    stopwatchTime += 1
+    // change what shows up on the page
+    stopwatchText.textContent = stopwatchTime
+}
+
+// EXERCISES
+
+// 1. Create two buttons in the html, one for blue, one for red
+
+// 2. When the red button is clicked, change the color of the <h1> to "red"
+
+const redButton = document.querySelector("#red-button")
+
+redButton.addEventListener("click", changeToRed)
+
+function changeToRed() {
+    const h1 = document.querySelector("h1")
+    h1.style.color = "red"
+}
+
+// 3. When the blue button is clicked, change the color of the <h1> to "blue"
+
+const blueButton = document.querySelector("#blue-button")
+
+blueButton.addEventListener("click", changeToBlue)
+
+function changeToBlue() {
+    const h1 = document.querySelector("h1")
+    h1.style.color = "blue"
+}
+
+// 4. BONUS - Instead of just changing the color of the h1, change the color for ALL the text on the page
+
+// const redButton = document.querySelector("#red-button")
+
+// redButton.addEventListener("click", changeToRed)
+
+// function changeToRed() {
+//     const allElements = document.querySelectorAll("*")
+//     allElements.forEach( element => element.style.color = "red")
+// }
